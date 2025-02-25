3dc6e8fcdb937e49f1b0c4351669c3162137c717
dasrajib <das.rajibdas@gmail.com>
2025-02-24 20:01:51 -0500

    projects api cahnges

--- src/main/java/com/adu/api/model/Project.java
+++ src/main/java/com/adu/api/model/Project.java
@@ -14,20 +14,20 @@ import org.springframework.data.relational.core.mapping.Table;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table("Projects")
+@Table("projects")
 public class Project  {
 
     @Id
     private Long project_id;
     private Long user_id;
     private Long address_id;
-    private Long basemodel_id;
-    private Long materialexterior_id;
-    private Long materialroof_id;
-    private Long materialwindowdoor_id;
-    private Long layoutentry_id;
-    private Long materialsolar_id;
-    private Long layoutkitchen_id;
+    private Long base_model_id;
+    private Long material_exterior_id;
+    private Long material_roof_id;
+    private Long material_window_door_id;
+    private Long layout_entry_id;
+    private Long material_solar_id;
+    private Long layout_kitchen_id;
     private Long upgrade_id;
     private String name;
     private String status;
